4e6c5233031fff138bf64970786ede6bc10e84e1
Ghislaine Laios <laios54@pm.me>
2024-02-21 15:17:37 +0800

    Update.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,54 @@
 use std::env;
 
 use actix_web::{App, get, HttpResponse, HttpServer, Responder, web};
-use log::{error, info};
-use sea_orm::Database;
+use anyhow::{anyhow, Context};
+use sea_orm::{Database, DatabaseConnection};
 
-use migration::MigratorTrait;
+use migration::{Migrator, MigratorTrait};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().expect(".env file is not found");
     env_logger::init();
 
-    let db_url = env::var("DATABASE_URL").expect("Environment variable DATABASE_URL is not set");
+    // Initialize the database (pool).
+
+    let db_url =
+        env::var("DATABASE_URL").context("Environment variable DATABASE_URL is not set")?;
 
     let db_coon = Database::connect(&db_url)
         .await
-        .expect("Can't connect to the database");
+        .context("Can't connect to the database")?;
 
-    db_coon.ping().await.expect("Failed to ping the database");
-
-    // TODO: https://www.sea-ql.org/SeaORM/docs/migration/running-migration/#checking-migration-status
-    let applied_migrations = migration::Migrator::get_applied_migrations(&db_coon)
+    db_coon
+        .ping()
         .await
-        .expect("Failed to get applied migrations");
+        .context("Failed to ping the database")?;
 
-    error!("{:}", &applied_migrations.len());
+    // Confirm the application of pending migrations.
+    let pending_migrations = Migrator::get_pending_migrations(&db_coon)
+        .await
+        .expect("Failed to get the pending migrations");
+    if !pending_migrations.is_empty() {
+        return Err(anyhow!("Pending migrations await application."));
+    }
 
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(AppState {
                 app_name: "actix demo".to_string(),
+                db_coon,
             }))
             .wrap(actix_web::middleware::Logger::default())
             .service(hello)
     })
     .bind(("127.0.0.1", 8067))
-    .unwrap()
+    .context("Fail to bind to 127.0.0.1:8067")?
     .run()
     .await
     .unwrap();
+
+    Ok(())
 }
 
 #[get("/hello")]
@@ -48,4 +58,5 @@ async fn hello(app_state: web::Data<AppState>) -> impl Responder {
 
 struct AppState {
     pub app_name: String,
+    pub db_coon: DatabaseConnection,
 }
